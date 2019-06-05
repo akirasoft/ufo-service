25cf9bdf43aa54d54394089d8f40f3086562d900
akirasoft <akirasoft@gmail.com>
2019-06-05 16:04:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+
+# UFO Service
+
+This service updates status LEDs on the Dynatrace UFO based on the state of a pipeline run.
+The service is subscribed to the following keptn events:
+
+- sh.keptn.events.deployment-finished
+- sh.keptn.events.evaluation-done
+- sh.keptn.events.tests-finished
+
+A deployment-finished event will result in the TOP row LEDs flashing GREEN (00ff00)
+A tests-finished event will result in the TOP row LEDs flashing BLUE (0000ff)
+An evaluation-done event will set LEDs based on the status of the evaluation. Pass will set LEDs to GREEN, Fail will set LEDs to RED (ff0000)
+
+## Installation
+
+To use this service, you must have a Dynatrace UFO accessible from your Kubernetes cluster. Additionally, you must have a secret defined with the IP address of your UFO:
+```
+kubectl -n keptn create secret generic ufo --from-literal="UFO_ADDRESS=<replacewithip>"
+```
+
+Afterwards, to install the service in your keptn installation checkout or copy the `ufo-service.yaml`.
+
+Then apply the `ufo-service.yaml` using `kubectl` to create the Dynatrace service and the subscriptions to the keptn channels.
+
+```
+kubectl apply -f ufo-service.yaml
+```
+
+Expected output:
+
+```
+service.serving.knative.dev/ufo-service created
+subscription.eventing.knative.dev/ufo-subscription-deployment-finished created
+subscription.eventing.knative.dev/ufo-subscription-tests-finished created
+subscription.eventing.knative.dev/ufo-subscription-evaluation-done created
+```
+
+## Verification of installation
+
+```
+$ kubectl get ksvc ufo-service -n keptn
+NAME            DOMAIN                               LATESTCREATED         LATESTREADY           READY     REASON
+ufo-service   ufo-service.keptn.x.x.x.x.xip.io   ufo-service-dd9km   ufo-service-dd9km   True
+```
+
+```
+$ kubectl get subscription -n keptn | grep ufo-subscription
+ufo-subscription-deployment-finished          True
+ufo-subscription-evaluation-done              True
+ufo-subscription-tests-finished               True
+$
+```
+
+When the next event is sent to any of the subscribed keptn channels the UFO LEDs should be adjusted accordingly.
+
+## Uninstall service
+
+To uninstall the dynatrace service and remove the subscriptions to keptn channels execute this command.
+
+```
+kubectl delete -f ufo-service.yaml
+````
---
+++ main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type keptnEvent struct {
+	Specversion     string `json:"specversion"`
+	Type            string `json:"type"`
+	Source          string `json:"source"`
+	ID              string `json:"id"`
+	Time            string `json:"time"`
+	Datacontenttype string `json:"datacontenttype"`
+	Shkeptncontext  string `json:"shkeptncontext"`
+	Data            struct {
+		Githuborg          string `json:"githuborg"`
+		Project            string `json:"project"`
+		Teststrategy       string `json:"teststrategy"`
+		Deploymentstrategy string `json:"deploymentstrategy"`
+		Stage              string `json:"stage"`
+		Service            string `json:"service"`
+		Image              string `json:"image"`
+		Tag                string `json:"tag"`
+		EvaluationPassed   bool   `json:"evaluationpassed,omitempty"`
+	} `json:"data"`
+}
+
+var (
+	infoLog  *log.Logger
+	errorLog *log.Logger
+)
+
+var ufoAddress string
+
+//Logging : sets up info and error logging
+func Logging(infoLogger io.Writer, errorLogger io.Writer) {
+	infoLog = log.New(infoLogger, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog = log.New(errorLogger, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func keptnHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var event keptnEvent
+	err := decoder.Decode(&event)
+	if err != nil {
+		fmt.Println("Error while parsing JSON payload: " + err.Error())
+		return
+	}
+	if event.Type == "sh.keptn.events.deployment-finished" {
+		ufoRow := "top"
+		ufoColor := "00ff00"
+		infoLog.Println("Trying to talk to UFO at " + ufoAddress + " setting " + ufoRow + " to " + ufoColor)
+		sendUFORequest(ufoAddress, ufoRow, ufoColor, true)
+	} else if event.Type == "sh.keptn.events.evaluation-done" {
+		if event.Data.EvaluationPassed {
+			ufoRow := "bottom"
+			ufoColor := "00ff00"
+			infoLog.Println("Trying to talk to UFO at " + ufoAddress + " setting " + ufoRow + " to " + ufoColor)
+			sendUFORequest(ufoAddress, ufoRow, ufoColor, false)
+		} else {
+			ufoRow := "bottom"
+			ufoColor := "ff0000"
+			infoLog.Println("Trying to talk to UFO at " + ufoAddress + " setting " + ufoRow + " to " + ufoColor)
+			sendUFORequest(ufoAddress, ufoRow, ufoColor, false)
+		}
+	} else if event.Type == "sh.keptn.events.tests-finished" {
+		ufoRow := "top"
+		ufoColor := "0000ff"
+		infoLog.Println("Trying to talk to UFO at " + ufoAddress + " setting " + ufoRow + " to " + ufoColor)
+		sendUFORequest(ufoAddress, ufoRow, ufoColor, true)
+	}
+}
+
+func sendUFORequest(ufoAddress string, ufoRow string, ufoColor string, morph bool) {
+	url := "http://" + ufoAddress + "/api?" + ufoRow + "_init&" + ufoRow + "=0|15|" + ufoColor
+	urlmorph := "http://" + ufoAddress + "/api?" + ufoRow + "_init&" + ufoRow + "=0|15|" + ufoColor + "&top_morph=30|10"
+	var preparedurl string
+	if morph {
+		preparedurl = urlmorph
+	} else {
+		preparedurl = url
+	}
+	req, err := http.NewRequest("GET", preparedurl, nil)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		errorLog.Println("Error while sending request to UFO: " + err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	infoLog.Println("Response Status:" + resp.Status)
+}
+
+func ufoInit(ufoAddress string) {
+	initURL := "http://" + ufoAddress + "/api?top_init&bottom_init"
+	infoLog.Println("Trying to initialize UFO at " + ufoAddress)
+	resp, err := http.Get(initURL)
+	if err != nil {
+		errorLog.Println("Error while sending request to UFO: " + err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	infoLog.Println("Response Status:" + resp.Status)
+}
+
+func main() {
+	Logging(os.Stdout, os.Stderr)
+	ufoAddress = os.Getenv("UFO_ADDRESS")
+	if ufoAddress == "" {
+		errorLog.Println("No UFO address defined")
+		return
+	}
+	ufoInit(ufoAddress)
+
+	http.HandleFunc("/", keptnHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Print("UFO service started.")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
